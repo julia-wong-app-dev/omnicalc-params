69dd419fc100732f58b2038ab2dc5b0518cdc7b0
Julia Wong <jwong8@chicagobooth.edu>
2019-05-14 06:31:09 +0000

    flexible_random_number

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -11,4 +11,31 @@ class CalcController < ApplicationController
         render("flex_templates/square.html.erb")
         
     end
+    
+    def flex_square_root
+        @b_number = params.fetch("kangaroo").to_f
+        @square_root = @b_number**0.5
+        
+        render("flex_templates/square_root.html.erb")
+    end
+    
+    def flex_payment
+        @rate = params.fetch("basis_points").to_f
+        @r = helpers.number_to_percentage(@rate/100, precision:2)
+        @years = params.fetch("number_of_years").to_i
+        @present_value = params.fetch("present_value").to_i
+        @pv = helpers.number_to_currency(@present_value, precision:0)
+        @payment = (@rate/12/10000 * @present_value)/(1-((1+@rate/12/10000)**(-(@years*12))))
+        @pmt = helpers.number_to_currency(@payment, precision:2)
+        
+        render("flex_templates/payment.html.erb")
+    end
+    
+    def random
+        @low_end = params.fetch("low_end").to_i
+        @high_end = params.fetch("high_end").to_i
+        @random_number = Random.rand(@low_end...@high_end)
+        
+        render("flex_templates/random.html.erb")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-match("/flexible/square/:zebra", { :controller => "calc", :action => "flex_square", :via => "get"})
+match("/flexible/square/:zebra", { :controller => "calc", :action => "flex_square", :via => "get" })
+
+match("/flexible/square_root/:kangaroo", { :controller => "calc", :action => "flex_square_root", :via => "get" })
+
+match("/flexible/payment/:basis_points/:number_of_years/:present_value", { :controller => "calc", :action => "flex_payment", :via => "get" })
+
+match("/flexible/random/:low_end/:high_end", { :controller => "calc", :action => "random", :via => "get" })
 
 end
