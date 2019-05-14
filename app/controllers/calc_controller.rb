24b1adbe2a503a287a87dea144335f87dfd00c6f
Julia Wong <jwong8@chicagobooth.edu>
2019-05-14 04:48:53 +0000

    dynamic-square

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -1,5 +1,14 @@
 class CalcController < ApplicationController
     def flex_square
+        
+        # Rails, behind the scenes, creates a variable called 'params' and assigns:
+        #  Parameters: {"zebra"=>"42"}
+        # Now, we get to use it
+        
+        @a_number = params.fetch("zebra").to_i
+        @squared_number = @a_number**2
+        
         render("flex_templates/square.html.erb")
+        
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-match("/flexible/square/5", { :controller => "calc", :action => "flex_square", :via => "get"})
+match("/flexible/square/:zebra", { :controller => "calc", :action => "flex_square", :via => "get"})
 
 end
